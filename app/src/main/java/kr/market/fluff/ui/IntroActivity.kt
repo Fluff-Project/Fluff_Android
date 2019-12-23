3d658fe4d8f0a12b3306676ab4f9fea7fbd4b0a5
CHJUNE-android <chjune0205@gmail.com>
2019-12-24 01:01:30 +0900

    매거진 추가 및 하단 탭 수정

--- app/src/main/java/kr/market/fluff/ui/IntroActivity.kt
+++ app/src/main/java/kr/market/fluff/ui/IntroActivity.kt
@@ -13,7 +13,7 @@ class IntroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_intro)
         init()
     }
