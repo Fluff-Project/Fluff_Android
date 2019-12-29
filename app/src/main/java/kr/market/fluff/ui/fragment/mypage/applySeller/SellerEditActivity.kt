060a1a21403e43e451a7202ac62cb95848c35f3e
hyesun <nbn0809@naver.com>
2019-12-30 07:19:10 +0900

    [혜선] 플럽 신청전,후 뷰 ui 완료

---
+++ app/src/main/java/kr/market/fluff/ui/fragment/mypage/applySeller/SellerEditActivity.kt
@@ -0,0 +1,13 @@
+package kr.market.fluff.ui.fragment.mypage.applySeller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.market.fluff.R
+
+class SellerEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller_edit)
+    }
+}
--- app/src/main/java/kr/market/fluff/ui/myStyle/MyStyleViewHolder.kt
+++ app/src/main/java/kr/market/fluff/ui/myStyle/MyStyleViewHolder.kt
@@ -1,7 +1,9 @@
 package kr.market.fluff.ui.myStyle
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.PorterDuff
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
@@ -10,16 +12,29 @@ import kr.market.fluff.R
 import kr.market.fluff.data.MyStyleData
 
 class MyStyleViewHolder (view: View) : RecyclerView.ViewHolder(view){
-    var count: Int = 0
-    var min: Int = 3
     val img_my_style: ImageView = view.findViewById(R.id.img_my_style)
     val img_my_style_checked: ImageView = view.findViewById(R.id.img_my_style_checked)
+    private val limit_count: Int = 3
 
-    fun bind(data: MyStyleData){
+    fun bind(data: MyStyleData,context: Context){
         Glide.with(itemView).load(data.img_url).into(img_my_style)
+        var ctx = context as MyStyleActivity
         img_my_style.setOnClickListener {
-            img_my_style.setColorFilter(Color.parseColor("#88000000"), PorterDuff.Mode.SRC_OVER)
-            img_my_style_checked.visibility = View.VISIBLE
+            if(img_my_style_checked.visibility == View.VISIBLE){
+                img_my_style.setColorFilter(Color.parseColor("#00ffffff"), PorterDuff.Mode.SRC_OVER)
+                img_my_style_checked.visibility = View.INVISIBLE
+                ctx.click_count--
+            }else {
+                img_my_style.setColorFilter(Color.parseColor("#88000000"), PorterDuff.Mode.SRC_OVER)
+                img_my_style_checked.visibility = View.VISIBLE
+                ctx.click_count++
+            }
+            if(ctx.click_count>=limit_count) {
+                ctx.changeBtn(true)
+            }else{
+                ctx.changeBtn(false)
+            }
         }
     }
+
 }
