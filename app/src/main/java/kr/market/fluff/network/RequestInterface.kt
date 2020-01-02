30a1f36bae98edf964cb4db24af240628cc9d069
CHJUNE-android <chjune0205@gmail.com>
2020-01-03 07:10:13 +0900

    [호준] 구매 플로우 틀 완성

--- app/src/main/java/kr/market/fluff/network/RequestInterface.kt
+++ app/src/main/java/kr/market/fluff/network/RequestInterface.kt
@@ -134,7 +134,7 @@ interface RequestInterface {
     ) : Call<BaseResponse<ArrayList<CartListResponse>>>
     data class CartListResponse(
         val userName : String,
-        val Img : String,
+        val Img : ArrayList<String>,
         val goodsId : String,
         val goodsName : String,
         val price : Long
